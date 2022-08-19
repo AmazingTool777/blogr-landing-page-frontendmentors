cf1be01467ba8d28485fbec92fb7dd64b312550d
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-08-19 16:49:36 +0300

    Dropdowns in the page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+
+import setDropdown from "./dropdown.js";
+
+// Set navbar dropdown
+setDropdown("navbar-menu", {
+	menuClassName: "hide-mobile",
+});
+
+// Set connect sublinks dropdown
+setDropdown("connect-sublinks");
