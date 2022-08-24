9fed650db35df6914759e0f800008a89aea00f98
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-08-24 15:54:51 +0300

    Explain the dropdown javascript code

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,13 @@
 // Default options
 const defaultOptions = {
+	// ClassName of the toggle
 	toggleClassName: "toggle-open",
+	// Classname of the dropdown menu
 	menuClassName: "dropdown-open",
 };
 
-// Function that toggles the classNames of dropdowns on click
+// Function that toggles the classNames of dropdowns between open/closed states
 function toggleDropdown(toggle, menu, options) {
-	// e.stopPropagation();
 	toggle.classList.toggle(options.toggleClassName);
 	menu.classList.toggle(options.menuClassName);
 }
@@ -16,11 +17,22 @@ export default function setDropdown(toggleTarget, _options = defaultOptions) {
 	const options = { ...defaultOptions, ..._options };
 	const toggle = document.querySelector(`[data-toggle="${toggleTarget}"]`);
 	const menu = document.getElementById(toggleTarget);
+	// Open state of the dropdown
 	let isOpen = false;
+
+	/* 
+	* On toggle click, the open state of the dropdown is toggled
+	*/
 	toggle.onclick = () => {
 		toggleDropdown(toggle, menu, options);
 		isOpen = !isOpen;
 	};
+
+	/* 
+	* If anything outside of the menu that is different from the toggle is clicked
+	* and the dropdown is open,
+	* Then we should close the dropdown
+	*/
 	window.addEventListener("click", (e) => {
 		if (isOpen && !menu.contains(e.target) && !toggle.isSameNode(e.target) && !toggle.contains(e.target)) {
 			toggleDropdown(toggle, menu, options);
